b4ac642da5a0b20aa690b8509436728c72b7a1eb
Teona-tech <teoforchxidze30@gmail.com>
2021-12-20 13:11:14 +0400

    arr rotation

---
+++ Arrays/RotateArray1D.js
@@ -0,0 +1,41 @@
+/**
+ * Rotate each element in array by steps
+ *  @param {number[]} arr array
+ *  @param {number} steps steps 
+ *  @return {number[]} The rotated array
+ */
+
+ function rotateArr(arr,steps){
+    if (arr.length === 0){
+        return arr;
+    }
+
+    if (steps === 0){
+        return arr;
+    }
+
+    steps%=arr.length;
+
+    if (steps < 0){
+        steps+=arr.length;
+    }
+
+
+    function reverseArr(start,end){
+        while(start<end){
+          [arr[start],arr[end]]=[arr[end],arr[start]];
+          start++;
+          end--;
+        }
+    }
+
+    reverseArr(0,arr.length-1);
+
+    reverseArr(0,steps-1);
+
+    reverseArr(steps,arr.length-1);
+
+
+    return arr;
+
+}
